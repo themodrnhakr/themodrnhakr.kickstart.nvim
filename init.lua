3c508dd1e60ddd342806597cf95c01478f599fb4
themodernhakr <ehrumsey@gmail.com>
2024-04-29 16:02:16 -0500

    fix color-theme config

--- init.lua
+++ init.lua
@@ -225,6 +225,8 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  -- ## MY PLUGINS ##
+  { 'savq/melange-nvim' },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
@@ -778,7 +780,7 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'melange',
+    'melange-nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
